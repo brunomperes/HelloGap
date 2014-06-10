d1d386b364e8abe16a3e9be649c9b4537e72f71b
Bruno <bruno@dexterity.dcs.gla.ac.uk>
2014-06-10 15:44:08 +0100

    Part 3 of tutorial

--- src/com/example/hellogap/MainActivity.java
+++ src/com/example/hellogap/MainActivity.java
@@ -1,28 +1,21 @@
 package com.example.hellogap;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
+import org.apache.cordova.DroidGap;
+
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends DroidGap {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
+        super.loadUrl("file:///android_asset/www/index.html");
     }
 
 
